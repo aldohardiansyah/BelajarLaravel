6bc6d6f35480ddbe326488e161b1c1ed0c0ebcd4
aldohardiansyah <aldohardiansyah@gmail.com>
2017-12-19 18:03:52 +0700

    version 2.0

---
+++ resources/views/belajar/hello.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello World</title>
+  </head>
+  <style>
+      html, body {
+          background-color: #fff;
+          color: #636b6f;
+          font-family: 'Raleway', sans-serif;
+          font-weight: 100;
+          height: 100vh;
+          margin: 1%;}
+  </style>
+  <body>
+    <h3> Welcome To The Jungle <h3>
+  </body>
+</html>
